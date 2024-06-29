525e7dd5374ad38b49ce7dd84d4918f19a2b9e61
Ehsan <ehsanmelm82@gmail.com>
2024-06-29 23:37:00 +0330

    feat : setting up admin page for all models

--- DijiShop/urls.py
+++ DijiShop/urls.py
@@ -19,6 +19,7 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('' , include('shop.urls')),
     path(r'auth/', include('djoser.urls')),
     path(r'auth/', include('djoser.urls.jwt')),
     
--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import SellerModel , StoreModel , StuffModel
 
 # Register your models here.
+
+admin.site.register(SellerModel)
+admin.site.register(StoreModel)
+admin.site.register(StuffModel)
